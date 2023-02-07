d2826d080000f74eb907ef9784177a2a38bff098
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-02-07 19:04:13 +0000

    fix: sidebar links

--- vitepress/docs/src/custom/theme/index.ts
+++ vitepress/docs/src/custom/theme/index.ts
@@ -29,64 +29,31 @@ const theme = {
             collapsible: true,
             items: [
                 { text: "Introduction", link: "/basicsOfProgramming" },
-                { text: "Things to know before you start", link: "/getting_started/things_to_know" },
             ],
         },
         {
-            text: "How to Build",
+            text: "Code Guides",
             collapsible: true,
             items: [
-                { text: "Full Build Guide", link: "/how_to_build/full_build" },
-                { text: "Part List", link: "/how_to_build/parts_list" },
-                { text: "How to Create your own Camera Mount", link: "/how_to_build/creating_your_own_mount" },
+                { text: "Combining Sketches", link: "/combiningSketches" },
+                { text: "Combining Sketches", link: "/millisVsDelay" },
             ],
         },
         {
-            text: "Firmware Guide",
+            text: "Hardware Guides",
             collapsible: true,
             items: [
-                { text: "Introduction", link: "/firmware_guide/firmware" },
-                { text: "Setup Visual Studio Code environment", link: "/firmware_guide/setup_vscode" },
-                { text: "Configuring Firmware", link: "/firmware_guide/configure_firmware" },
-                { text: "Upload Firmare", link: "/firmware_guide/upload_firmware" },
-                { text: "MDNS", link: "/firmware_guide/mdns" },
-                { text: "REST API", link: "/firmware_guide/rest_api" },
+                { text: "BreadBoards", link: "/breadboard" },
+                { text: "Buttons", link: "/buttons" },
+                { text: "Resistors", link: "/ledResistors" },
+                { text: "Nine Volts", link: "/nineVolts" },
+
             ],
-        },
-        {
-            text: "Software Guide",
-            collapsible: true,
-            items: [
-                { text: "Installing and setting up the ETVR App", link: "/software_guide/eyetrackvr_app_guide" },
-                { text: "Building the app from source", link: "/software_guide/build_software" },
-            ],
-        },
-        {
-            text: "Miscellaneous",
-            collapsible: true,
-            items: [
-                { text: "VR Chat Avatar Setup", link: "/misc/vrc_avatar_setup" },
-                { text: "FAQ", link: "/misc/faq" },
-            ],
-        },
-        {
-            text: "Development",
-            collapsible: true,
-            items: [
-                { text: "Docs", link: "/development/docs/dev_docs" },
-            ],
-        },
-        {
-            text: "Archive",
-            collapsible: true,
-            items: [
-                { text: "Prepare to solder IR LED PCB V2's", link: "/archive/fox_ir_v2_build_instructions" },
-            ],
-        },
+        }
     ],
     socialLinks: [
-        { icon: 'github', link: 'https://github.com/RedHawk989/EyeTrackVR' },
-        { icon: 'discord', link: 'https://discord.gg/kkXYbVykZX' }
+        { icon: 'github', link: 'https://github.com/arduinodiscord/wiki' },
+        { icon: 'discord', link: 'https://discord.gg/arduino' }
     ]
 }
 
